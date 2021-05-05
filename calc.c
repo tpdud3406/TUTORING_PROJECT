b7be2398581c1acecb698334d4d7cd7d3ea13037
Jeong Seyoung <tpdud5187@gmail.com>
2021-05-04 23:46:24 -0700

    start

---
+++ calc.c
@@ -0,0 +1,7 @@
+#include "calc.h"
+
+//공학용 계산기의 작동 로직은 calc.c에서 구현한다.
+
+void calc(){
+    printf("it is cal.c\n");
+}
---
+++ calender.c
@@ -0,0 +1,7 @@
+#include "calender.h"
+
+//스케줄 관리 프로그램의 작동 로직은 calender.c에서 구현한다.
+
+void calender(){
+    printf("it is calender.c\n");
+}
