e6e1f82001db25d3583e25e5ab303781a753a681
Jeong Seyoung <tpdud5187@gmail.com>
2021-05-11 23:32:08 -0700

    연결리스트로 다시!

--- calender.h
+++ calender.h
@@ -1,21 +1,19 @@
 #include<stdio.h>
-/************************************************
- * calender.c에 필요한 헤더파일들은 현재 파일에 선언
- * calender.c에 구현될 함수들의 원형(proto type)은 현재 파일에 선언
- * 구조체 및 DEFINE 메크로도 현재 파일에 선언
- * ************************************************/
 
-typedef struct Calender {
-    int year;
-    int month;
-    int day;
-    char content[100];
-} Calender;
+typedef struct Node {
+    int data;
+    Node *next;
+    Node *prev;
+} Node;
 
-void calender(void);
-void input_schedule(void);
-void delete_schedule(void);
-void view_schedule(void);
-int schedule_bool(void);
-void schedule_sub_delete(Calender input);
-int year_check(int i);
+typedef struct LinkedList {
+    Node *head;
+    Node *tail;
+    int numofData;
+} List;
+
+void Append(int data); // list의 마지막에 새로운 data에 대한 Node 추가
+void Prepend(int data); // list의 처음에 새로운 data에 대한 Node 추가
+int Length(void); // list에 있는 Node의 수를 return
+void Print(void); // 앞에서부터 출력
+void PrintReverse(void); // 뒤에서부터 출력
